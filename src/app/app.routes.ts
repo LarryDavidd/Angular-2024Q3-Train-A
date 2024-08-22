3fc7c0cf5062d50a719d9290241005cda211b25f
Ekaterina_Ch <70838054+Kianeay@users.noreply.github.com>
2024-08-22 18:22:13 +0300

    feat: add login module structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from 'auth/components/login/login.component';
 import { SignUpComponent } from 'auth/components/sign-up/sign-up.component';
 
-export const routes: Routes = [{ path: 'signup', component: SignUpComponent }];
+export const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'signin', component: LoginComponent }
+];
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpModule } from './components/sign-up/sign-up.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginModule } from './components/login/login.module';
 
 @NgModule({
-  imports: [CommonModule, SignUpModule, HttpClientModule]
+  imports: [CommonModule, SignUpModule, HttpClientModule, LoginModule]
 })
 export class AuthModule {}
