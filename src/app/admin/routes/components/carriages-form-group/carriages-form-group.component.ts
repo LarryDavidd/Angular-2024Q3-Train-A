851ce0b2b708b8e2dd44fcaf913fb0c1c96e0273
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-08-23 20:42:02 +0300

    feat: add carriages form group

--- src/app/admin/routes/components/carriages-form-group/carriages-form-group.component.ts
+++ src/app/admin/routes/components/carriages-form-group/carriages-form-group.component.ts
@@ -2,16 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CarriagesResponse } from 'admin/carriages/model/carriages.model';
 
 @Component({
-  selector: 'app-route-create-form',
+  selector: 'app-carriages-form-group',
   standalone: true,
   imports: [MatFormFieldModule, CommonModule, ReactiveFormsModule],
-  templateUrl: './route-create-form.component.html',
-  styleUrl: './route-create-form.component.scss'
+  templateUrl: './carriages-form-group.component.html',
+  styleUrl: './carriages-form-group.component.scss'
 })
-export class RouteCreateFormComponent {
-  @Input() itemsList!: string[];
+export class CarriagesFormGroupComponent {
+  @Input() carriages!: CarriagesResponse;
 
   form = new FormGroup({
     select: new FormArray([new FormControl('', [Validators.required])])
@@ -24,6 +25,10 @@ export class RouteCreateFormComponent {
   reset() {
     this.form.reset();
     this.form.controls.select.clear();
+    this.addSelectField();
+  }
+
+  onChange(): void {
     this.addSelectField();
   }
 }
--- src/app/admin/routes/components/stations-form-group/stations-form-group.component.ts
+++ src/app/admin/routes/components/stations-form-group/stations-form-group.component.ts
@@ -14,20 +14,11 @@ import { GetStationsResponse } from 'admin/stations/model/station.model';
 export class StationsFormGroupComponent implements OnInit {
   @Input() stations!: GetStationsResponse;
 
-  private lastSelectedStationID: number | null = null;
-
   stationsForSelect: GetStationsResponse[] = [];
 
   form = new FormGroup({
     select: new FormArray([new FormControl('', [Validators.required])])
   });
-
-  onChange(event: Event, index: number): void {
-    const selectElement = event.target as HTMLSelectElement;
-    const selectedOption = selectElement.options[selectElement.selectedIndex];
-    const selectedId = selectedOption.id;
-    this.setStations = { id: selectedId, index };
-  }
 
   addSelectField() {
     this.form.controls.select.push(new FormControl('', [Validators.required]));
@@ -43,46 +34,28 @@ export class StationsFormGroupComponent implements OnInit {
     return this.stations.find((station) => station.id === id);
   }
 
-  get getStationByLastSelectedStationID() {
-    return this.stations.find((station) => station.id === this.lastSelectedStationID);
+  onChange(event: Event, index: number): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const selectedId = selectedOption.id;
+    this.setStations = { id: selectedId, index };
   }
 
   set setStations({ id, index }: { id: string; index: number }) {
-    // this.form.controls.select.controls = this.form.controls.select.controls.splice(index);
-    this.addSelectField();
+    this.form.controls.select.controls = this.form.controls.select.controls.splice(0, index + 1);
+    this.stationsForSelect = this.stationsForSelect.slice(0, index + 1);
 
-    console.log(this.stationsForSelect, id, index);
-    // this.stationsForSelect = this.stationsForSelect.splice(index + 1);
-    console.log(this.stationsForSelect, id, index);
+    this.addSelectField();
 
     this.stationsForSelect[index + 1] = [];
 
-    const item = this.getStationById(Number(id));
+    const station = this.getStationById(Number(id));
 
     this.stations.forEach((defaultStation) => {
-      item?.connectedTo.forEach((connectedStation) => {
+      station?.connectedTo.forEach((connectedStation) => {
         if (defaultStation.id === connectedStation.id) this.stationsForSelect[index + 1].push(defaultStation);
       });
     });
-  }
-
-  get getStations() {
-    if (this.form.controls.select.length === 1) {
-      return this.stations;
-    } else if (this.lastSelectedStationID) {
-      const item = this.getStationByLastSelectedStationID;
-
-      const newStationsList: GetStationsResponse = [];
-
-      this.stations.forEach((defaultStation) => {
-        item?.connectedTo.forEach((connectedStation) => {
-          if (defaultStation.id === connectedStation.id) newStationsList.push(defaultStation);
-        });
-      });
-
-      return newStationsList;
-    }
-    return [];
   }
 
   ngOnInit() {
