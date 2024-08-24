cc943d785712108ba008632b9b022320578b2779
Ekaterina_Ch <70838054+Kianeay@users.noreply.github.com>
2024-08-24 14:40:15 +0300

    feat: add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from 'auth/auth.guard';
 import { LoginComponent } from 'auth/components/login/login.component';
 import { SignUpComponent } from 'auth/components/sign-up/sign-up.component';
+import { HomeComponent } from 'home/home.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignUpComponent },
-  { path: 'signin', component: LoginComponent }
+  { path: 'signin', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent }
 ];
