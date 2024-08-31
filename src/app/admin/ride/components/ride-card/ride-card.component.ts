a590780a4285a250e3916b233491931d88a078aa
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-08-31 04:22:59 +0300

    feat: add stiles on ride-card

--- src/app/admin/ride/components/ride-card/ride-card.component.ts
+++ src/app/admin/ride/components/ride-card/ride-card.component.ts
@@ -5,8 +5,9 @@ import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
 import { Schedule } from 'admin/ride/model/ride.model';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 interface SegmentForm {
   arrivalTime: FormControl<string | null>;
@@ -21,7 +22,7 @@ interface FormGroupInterface {
 @Component({
   selector: 'app-ride-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButton, MatIcon],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButton, MatIcon, MatDatepickerModule, MatInput],
   templateUrl: './ride-card.component.html',
   styleUrl: './ride-card.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
@@ -16,6 +17,7 @@ import { routes } from 'app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideNativeDateAdapter(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
