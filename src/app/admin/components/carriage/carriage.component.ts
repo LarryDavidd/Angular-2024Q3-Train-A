bd5cc90086a24365d3ffc3fbb2ac84755c55c9b6
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-09-02 09:37:31 +0200

    feat: change styles, add seats numeration

--- src/app/admin/components/carriage/carriage.component.ts
+++ src/app/admin/components/carriage/carriage.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carriage',
+  styleUrl: './carriage.component.scss',
   templateUrl: './carriage.component.html'
 })
 export class CarriageComponent {
